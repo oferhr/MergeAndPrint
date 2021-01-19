5be020509ec22d9c12f5dfc9ac4abad1534bb5d6
Ofer Harari <offa23@hotmail.com>
2021-01-19 18:48:43 +0000

    Version 1.0.2 Setting license
    git-tfs-id: [https://oferh.visualstudio.com/DefaultCollection]$/Projects/MergeAndPrint;C139

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace MergeAndPrint
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            IronPdf.License.LicenseKey = "IRONPDF.ERANMOR.IRO210119.6673.61137.911012-E4A858C1E3-BVFV36ZMBODS75L-ZO7KIF2CSE6J-HSFYXRNZBPLL-YDMZFGUBQ6CZ-BRTEE3GQAWOA-NPJWTO-LDJM7U3RQN2DUA-PRO.1DEV.1YR-BJNCXP.RENEW.SUPPORT.19.JAN.2022";
             Application.Run(new Form1());
         }
     }
