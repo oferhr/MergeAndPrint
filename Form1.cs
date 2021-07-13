3485b5d4814e108d1a9dafb9b82ccd1266da8547
Ofer Harari <offa23@hotmail.com>
2021-07-13 19:37:59 +0000

    dg
    git-tfs-id: [https://oferh.visualstudio.com/DefaultCollection]$/Projects/MergeAndPrint;C173

--- Form1.cs
+++ Form1.cs
@@ -200,7 +200,7 @@ namespace MergeAndPrint
                 }
                 
             }
-            txtDetails.Clear();
+            //txtDetails.Clear();
             updateScreen();
             updateDirectories();
             Clean();
